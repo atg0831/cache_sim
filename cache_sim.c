574ef986eda57be14a0fb2dbfe083326c51dac41
atg0831 <atg0831@naver.com>
2021-07-19 12:57:15 +0900

    Delete unless file and modify code

--- cache_sim.c
+++ cache_sim.c
@@ -10,27 +10,30 @@
 #define ADDR long long
 #define BOOL char
 
-typedef struct _MEMACCESS {
+typedef struct _MEMACCESS
+{
 	ADDR addr;
 	BOOL is_read;
 } MEMACCESS;
 
-typedef struct _Cache {
+typedef struct _Cache
+{
 	int valid;
 	long long tag;
 	long long data;
 	int counter;
-}Cache;
+} Cache;
 
-
-
-typedef enum _RPL { LRU = 0, RAND = 1 } RPL;
+typedef enum _RPL
+{
+	LRU = 0,
+	RAND = 1
+} RPL;
 
 //misc. function
-FILE* fp = 0;
+FILE *fp = 0;
 char trace_file[100] = "memtrace.trc";
-BOOL read_new_memaccess(MEMACCESS*);  //read new memory access from the memory trace file (already implemented)
-
+BOOL read_new_memaccess(MEMACCESS *); //read new memory access from the memory trace file (already implemented)
 
 //configure the cache
 void init_cache(int cache_size, int block_size, int assoc, RPL repl_policy);
@@ -41,24 +44,8 @@ BOOL isHit(ADDR addr, RPL repl_policy, int assoc);
 //insert a cache block for a memory access
 ADDR insert_to_cache(ADDR addr, RPL repl_policy, int assoc);
 
-
 //print the simulation statistics
 void print_stat(int cache_size, int block_size, int assoc, RPL rep_policy);
-/*long long my_pow(int base, int exp)
-{
-	long long res = 1;
-	while(exp)
-		{
-		if (exp & 1)
-			res *= base;
-		exp >>= 1;
-		base *= base;
-		}
-
-	return res;
-}
-*/
-
 
 Cache **cache;
 int tag_bits;
@@ -72,8 +59,9 @@ long long add_offset;
 long long tag_mask;
 long long index_mask;
 long long offset_mask;
+
 //main
-int main(int argc, char*argv[])
+int main(int argc, char *argv[])
 {
 	int cache_size = 1024;
 	int assoc = 16;
@@ -84,8 +72,10 @@ int main(int argc, char*argv[])
 	*  Read through command-line arguments for options.
 	*/
 	//srand((unsigned)time(NULL));
-	for (i = 1; i < argc; i++) {
-		if (argv[i][0] == '-') {
+	for (i = 1; i < argc; i++)
+	{
+		if (argv[i][0] == '-')
+		{
 			if (argv[i][1] == 's')
 				cache_size = atoi(argv[i + 1]);
 
@@ -97,7 +87,6 @@ int main(int argc, char*argv[])
 
 			if (argv[i][1] == 'f')
 				strcpy(trace_file, argv[i + 1]);
-
 
 			if (argv[i][1] == 'r')
 			{
@@ -123,35 +112,29 @@ int main(int argc, char*argv[])
 
 	init_cache(cache_size, block_size, assoc, repl_policy);
 
-
 	//rand_assoc_count = assoc;
 	while (1)
 	{
 		MEMACCESS new_access;
 
-		BOOL success = read_new_memaccess(&new_access);  //read new memory access from the memory trace file
+		BOOL success = read_new_memaccess(&new_access); //read new memory access from the memory trace file
 
-		if (success != TRUE)   //check the end of the trace file
+		if (success != TRUE) //check the end of the trace file
 			break;
-
 
 		tag_mask = (long long)pow(2, tag_bits) - 1;
 		index_mask = (long long)pow(2, index_bits) - 1;
 		offset_mask = (long long)pow(2, byte_offset_bits) - 1;
 
 		add_tag = (new_access.addr >> (index_bits + byte_offset_bits)) & tag_mask;
-		add_index = (new_access.addr >> (byte_offset_bits))&index_mask;
+		add_index = (new_access.addr >> (byte_offset_bits)) & index_mask;
 
 		add_offset = new_access.addr & offset_mask;
 
-
-		if (isHit(new_access.addr, repl_policy, assoc) == FALSE)   //check if the new memory access hit on the cache
+		if (isHit(new_access.addr, repl_policy, assoc) == FALSE) //check if the new memory access hit on the cache
 		{
-			insert_to_cache(new_access.addr, repl_policy, assoc);  //if miss, insert a cache block for the memory access to the cache
+			insert_to_cache(new_access.addr, repl_policy, assoc); //if miss, insert a cache block for the memory access to the cache
 		}
-
-
-
 	}
 
 	// print statistics here
@@ -163,11 +146,10 @@ int main(int argc, char*argv[])
 	return 0;
 }
 
-
 /*
  * read a new memory access from the memory trace file
  */
-BOOL read_new_memaccess(MEMACCESS* mem_access)
+BOOL read_new_memaccess(MEMACCESS *mem_access)
 {
 	ADDR access_addr;
 	char access_type[10];
@@ -182,7 +164,6 @@ BOOL read_new_memaccess(MEMACCESS* mem_access)
 		{
 			fprintf(stderr, "error opening file");
 			exit(2);
-
 		}
 	}
 
@@ -200,71 +181,60 @@ BOOL read_new_memaccess(MEMACCESS* mem_access)
 		else
 			mem_access->is_read = FALSE;
 
-
 		return TRUE;
 	}
 	else
 		return FALSE;
-
 }
-
 
 void init_cache(int cache_size, int block_size, int assoc, RPL repl_policy)
 {
 	int set_index;
 	set_index = (cache_size / block_size) / assoc;
 
-
-	cache = (Cache **)malloc(sizeof(*cache)*set_index);
+	cache = (Cache **)malloc(sizeof(*cache) * set_index);
 	int i = 0, j = 0, k = 0;
 
 	for (i = 0; i < set_index; i++)
-		cache[i] = (Cache *)malloc(sizeof(**cache)*assoc);
+		cache[i] = (Cache *)malloc(sizeof(**cache) * assoc);
 
-
-
-
-
-	if (repl_policy == 0) {
-		for (i = 0; i < set_index; i++) {
+	if (repl_policy == 0)
+	{
+		for (i = 0; i < set_index; i++)
+		{
 			k = 0;
-			for (j = 0; j < assoc; j++) {
-				(cache[i][j]).counter = k++;	//LRU counter 0부터 assoc-1까지 각각 초기화
+			for (j = 0; j < assoc; j++)
+			{
+				(cache[i][j]).counter = k++; //LRU counter 0부터 assoc-1까지 각각 초기화
 
 				cache[i][j].valid = 0;
 				//cache[i][j].tag = 0;//valid는 전부 0으로 초기화
 			}
-
 		}
 	}
 	else
 	{
-		for (i = 0; i < set_index; i++) {
+		for (i = 0; i < set_index; i++)
+		{
 
-			for (j = 0; j < assoc; j++) {
+			for (j = 0; j < assoc; j++)
+			{
 
-				cache[i][j].valid = 0;		//valid는 0으로
+				cache[i][j].valid = 0; //valid는 0으로
 			}
-
 		}
-
 	}
 	index_bits = (int)log2((double)set_index);
 	byte_offset_bits = (int)log2((double)block_size);
 	tag_bits = 64 - (index_bits + byte_offset_bits);
-
-	// if((cache[i][j]).vaild==0 &&(cache[i][j]).counter==0
-
-
-
 }
 
 //check if the memory access hits on the cache
 BOOL isHit(ADDR addr, RPL repl_policy, int assoc)
 {
-
 	int i, j;
-	if (repl_policy == 0) {
+	if (repl_policy == 0)
+	{
 		for (j = 0; j < assoc; j++)
 		{
 			if (add_tag == cache[add_index][j].tag && (cache[add_index][j].valid) != 0)
@@ -275,10 +245,7 @@ BOOL isHit(ADDR addr, RPL repl_policy, int assoc)
 					if (cache[add_index][j].counter < cache[add_index][i].counter)
 					{
 						(cache[add_index][i].counter)--;
-
 					}
-
-
 				}
 
 				(cache[add_index][j].counter) = assoc - 1;
@@ -288,7 +255,7 @@ BOOL isHit(ADDR addr, RPL repl_policy, int assoc)
 			}
 
 			//else
-				//miss_count++;
+			//miss_count++;
 			//마지막 assoc다 돌았을때
 			else if ((j == assoc - 1) && (add_tag != cache[add_index][j].tag) || (cache[add_index][j].valid) == 0)
 			{
@@ -297,12 +264,8 @@ BOOL isHit(ADDR addr, RPL repl_policy, int assoc)
 				//insert_to_cache(addr,add_tag,add_index,add_offset);
 				return FALSE;
 			}
-
-
-
 		}
 	}
-
 
 	//RAND
 	else
@@ -312,14 +275,13 @@ BOOL isHit(ADDR addr, RPL repl_policy, int assoc)
 			if (add_tag == cache[add_index][j].tag && (cache[add_index][j].valid) != 0)
 			{
 
-
 				cache[add_index][j].valid = 1;
 				hit_count++;
 				break;
 			}
 
 			//else
-				//miss_count++;
+			//miss_count++;
 			else if ((j == assoc - 1) && (add_tag != cache[add_index][j].tag) || (cache[add_index][j].valid) == 0)
 			{
 
@@ -327,12 +289,8 @@ BOOL isHit(ADDR addr, RPL repl_policy, int assoc)
 				//insert_to_cache(addr,add_tag,add_index,add_offset);
 				return FALSE;
 			}
-
-
 		}
-
 	}
-
 
 	return TRUE;
 }
@@ -340,14 +298,12 @@ BOOL isHit(ADDR addr, RPL repl_policy, int assoc)
 //insert a cache block for a memory access
 ADDR insert_to_cache(ADDR addr, RPL repl_policy, int assoc)
 {
-
-
 	//int initial_rand = FALSE;
-
 	int random;
 	int i;
 
-	if (repl_policy == 0) {
+	if (repl_policy == 0)
+	{
 		for (i = 0; i < assoc; i++)
 		{
 			if (cache[add_index][i].counter == 0)
@@ -355,15 +311,6 @@ ADDR insert_to_cache(ADDR addr, RPL repl_policy, int assoc)
 				cache[add_index][i].valid = 1;
 				cache[add_index][i].tag = add_tag;
 				cache[add_index][i].counter = assoc - 1;
-
-				//	for (j = 0; j < assoc; j++) {
-
-					//	if (i != j)
-					//		(cache[add_index][j].counter)--;
-
-				//		//break;
-				//	}
-				//	break;
 			}
 			else
 				(cache[add_index][i].counter)--;
@@ -374,30 +321,11 @@ ADDR insert_to_cache(ADDR addr, RPL repl_policy, int assoc)
 	{
 
 		random = rand() % assoc;
-
-		/*if (rand_assoc_count > 0)		//초기에 assoc갯수만큼 일단 채워넣고(valid 0이므로)
-		{
-			while (cache[add_index][random].valid != 0)	//비어있는 공간찾을때까지
-				random = rand() % assoc;
-
-			cache[add_index][random].tag = add_tag;
-			cache[add_index][random].valid = 1;
-
-			rand_assoc_count--;
-		}
-		*/
-		//valid가 이제는 다 채워으니까 1이므로 random으로 replace
-		//else
-		//{
 		cache[add_index][random].tag = add_tag;
 		cache[add_index][random].valid = 1;
-		//}
-
-
 	}
 	return TRUE;
 }
-
 
 //print the simulation statistics
 void print_stat(int cache_size, int block_size, int assoc, RPL repl_policy)
@@ -415,9 +343,4 @@ void print_stat(int cache_size, int block_size, int assoc, RPL repl_policy)
 	printf("cache_hits : %d\n", hit_count);
 	printf("cache_misses : %d\n", miss_count);
 	printf("cache_miss_rate : %.2lf%\n", 100 * ((double)miss_count / (hit_count + miss_count)));
-
-
-
-
 }
-
